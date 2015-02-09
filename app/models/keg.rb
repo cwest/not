d5068134d7fb3d70106a75ebbbf6ad8ef3a0bb80
Casey West <casey.west@whitehatsec.com>
2015-02-08 20:06:25 -0500

    Create venue taps.

--- app/models/keg.rb
+++ app/models/keg.rb
@@ -1,4 +1,7 @@
 class Keg < ActiveRecord::Base
+  has_one :venue_tap,
+          inverse_of: :keg
+
   belongs_to :venue,
              inverse_of: :kegs
   belongs_to :beer,
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,6 +1,8 @@
 class Venue < ActiveRecord::Base
   has_many :kegs,
            inverse_of: :venue
+  has_many :venue_taps,
+           inverse_of: :venue
 
   belongs_to :organization,
              inverse_of: :venues
