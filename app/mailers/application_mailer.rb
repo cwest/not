75c6cf7d04683b13ef571269134f6df97c948173
Casey West <casey@geeknest.com>
2015-02-11 21:24:03 -0500

    Use markdown for emails.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,15 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
+
+  def mail(*args, &block)
+    if block_given?
+      super
+    else
+      super(*args) do |format|
+        format.text
+        format.html
+      end
+    end
+  end
 end
